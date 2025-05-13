117a9fe726dbeab4c2d08b71eeafb8421cc47eb4
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-05-12 22:09:37 -0400

    Ajout du clic pour copier la couleur

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
+console.log("Script is loaded!"); // Pour tester si le script est chargé
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Sélectionner toutes les couleurs dans chaque palette
   const colorDivs = document.querySelectorAll(".palette .colors div");
 
-  // Ajouter un événement de clic à chaque couleur
   colorDivs.forEach((colorDiv) => {
     colorDiv.addEventListener("click", function () {
-      // Récupérer le code hexadécimal de la couleur
       const colorCode = this.querySelector("span").getAttribute("data-code");
-
-      // Copier la couleur dans le presse-papiers
       navigator.clipboard
         .writeText(colorCode)
         .then(() => {
-          // Afficher une alerte en anglais que la couleur a été copiée
           alert(`Color ${colorCode} copied to clipboard!`);
         })
         .catch((err) => {
