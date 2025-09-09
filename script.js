11f6456d38586f2657049d90474c7309dac35a93
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-09-09 15:50:58 -0400

    Ajout_NavBar

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector(".nav-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  toggleBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
